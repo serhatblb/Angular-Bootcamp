333a01fb0e007062c2f44a7442e36110f3f6c1e5
serhatblb <serhatblb23@gmail.com>
2023-12-30 15:59:09 +0300

    Javasxript  work started

---
+++ 5_Javascript/template.js
@@ -0,0 +1,22 @@
+// Math Ödev
+// Kullanıcıda tarafından aldığımız 2 tane sayının (prompt)
+let numberOne = prompt("Enter First Number")
+let numberTwo = prompt("Enter Second Number")
+let sayilar = [numberOne, numberTwo]
+// s1-) Bu sayılardan en küçüğü nedir ? (min)
+console.log(Math.min(sayilar))
+// s2-) Bu sayılardan en büyüğü nedir ? (max)
+// s3-) Bu sayılardan en büyüğü karekökü nedir ? (sqrt(max))
+// s4-) Bu sayılardan en büyüğü karekökünün üste yuvarlıyalım nedir ? (ceil(sqrt(max)))
+// s5-) Bu sayılardan en küçüğü mutlak değeri nedir ? (abs(min))
+// s6-) Bu sayılardan en küçüğü alt taban en büyüğü üst taban şeklinde üstünü alalım(pow(min,max))
+
+// Örnek: Aşağıdaki örnekleri math ile çözelim ?
+// -5.9 sayıyının aşağıdaki işlemleri yaptıralım ?
+// 1-mutlak değeri alsın 5.9 (Math.abs)
+// 2-yukarı yuvarlama yapsın  6.0 (Math.ceil)
+// 3-karesini alsın 36.00 (Math.pow(sayı,2))
+// 4-çıkan sayının karekökü alsın (Math.sqrt(sayı))
+// 6-)çıkan sonucu 5 bölsün 6/5=1
+// 7-) iki sayı arasından karşılaştırma yapsın en küçüğünü alsın ve 1 ve 5
+// 8-) küçük sayı eğer tekse 3 eklesin çiftse 5 eklesin 
